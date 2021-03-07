9f2cf073d2a259e6237ab56c26c847b9523fc695
Kristian Nese <kristiannese@live.com>
2021-03-07 19:56:12 +0100

    kjh

--- azOpsSetup.ps1
+++ azOpsSetup.ps1
@@ -1,140 +1,155 @@
 ### PowerShell script to bootstrap AzOps into GitHub - as part of Enterprise-Scale landing zone setup 
 ### This script is developed and maintained by a set of diverse architects and engineers, part of the Azure Customer Architecture & Engineering team
 
-    [CmdletBinding()]
-    param (
-     [string]$PAToken,
-     [string]$GitHubUserNameOrOrg,
-     [string]$SpnObjectId,
-     [string]$AzureSpnPwd,
-     [string]$AzureTenantId,
-     [string]$AzureSubscriptionId
-    )
+[CmdletBinding()]
+param (
+ [string]$PAToken,
+ [string]$GitHubUserNameOrOrg,
+ [string]$SpnObjectId,
+ [string]$AzureSpnPwd,
+ [string]$AzureTenantId,
+ [string]$AzureSubscriptionId
+)
 
-    Install-Module -Name PowerShellForGitHub -Confirm:$false -Force
-    Import-Module -Name PowerShellForGitHub
+Install-Module -Name PowerShellForGitHub -Confirm:$false -Force
+Import-Module -Name PowerShellForGitHub
 
-    $SecureString = $PAToken | ConvertTo-SecureString -AsPlainText -Force
-    $Cred = New-Object System.Management.Automation.PSCredential "ignore", $SecureString
+$SecureString = $PAToken | ConvertTo-SecureString -AsPlainText -Force
+$Cred = New-Object System.Management.Automation.PSCredential "ignore", $SecureString
 
-    Write-Host "Authenticating to GitHub using PA token..."
+Write-Host "Authenticating to GitHub using PA token..."
 
-    Set-GitHubAuthentication -Credential $Cred
+Set-GitHubAuthentication -Credential $Cred
 
-    $ESLZGitHubOrg = "Azure"
-    $ESLZRepository = "AzOps-Accelerator"
-    $TestRepo = "fromScript"
-    #$TestRepo = $ESLZRepository
+$ESLZGitHubOrg = "Azure"
+$ESLZRepository = "AzOps-Accelerator"
+$TestRepo = "fromScript"
+#$TestRepo = $ESLZRepository
 
-    Write-Host "Creating Git repository from template.."
-    # Creating GitHub repository based on Enterprise-Scale
+Write-Host "Creating Git repository from template.."
+# Creating GitHub repository based on Enterprise-Scale
+
+$CheckExistence = Get-GitHubRepository -OwnerName $GitHubUserNameOrOrg -RepositoryName $TestRepo
+if ([string]::IsNullOrEmpty($CheckExistence))
+{
+    Write-Host "Repository does not exist in target organization/user - script will continue"
 
     Get-GitHubRepository -OwnerName $ESLZGitHubOrg `
-                         -RepositoryName $ESLZRepository | New-GitHubRepositoryFromTemplate `
-                         -TargetRepositoryName $TestRepo `
-                         -TargetOwnerName $GitHubUserNameOrOrg
+                     -RepositoryName $ESLZRepository | New-GitHubRepositoryFromTemplate `
+                     -TargetRepositoryName $TestRepo `
+                     -TargetOwnerName $GitHubUserNameOrOrg
 
-    # Get ESLZ template repository
-    $GetESLZRepoTemplate = @{
-        Uri     = "https://api.github.com/repos/$($ESLZGitHubOrg)/$($ESLZRepository)"
-        Headers = @{
-            Authorization = "Token $($PAToken)"
-        }
-        Method  = "GET"
+# Creating secret for the Service Principal into GitHub
+
+$ARMClient = [convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($SpnObjectId))
+$ARMSecret = [System.Net.NetworkCredential]::new("",$AzureSpnPwd).Password
+$ARMClientSecret = [convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($ARMSecret))
+$ARMTenant = [convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($AzureTenantId))
+$ARMSubscription = [convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($AzureSubscriptionId))
+
+Write-host "Getting GitHub Public Key to create new secrets..."
+$GetPublicKey = @{
+    Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/actions/secrets/public-key"
+    Headers = @{
+        Authorization = "Token $($PAToken)"
     }
-    $ESLZRepo = Invoke-RestMethod @GetESLZRepoTemplate
+    Method = "GET"
+}
+$GitHubPublicKey = Invoke-RestMethod @GetPublicKey
 
-    # Creating secret for the Service Principal into GitHub
-
-    $ARMClient = [convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($SpnObjectId))
-    $ARMSecret = [System.Net.NetworkCredential]::new("",$AzureSpnPwd).Password
-    $ARMClientSecret = [convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($ARMSecret))
-    $ARMTenant = [convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($AzureTenantId))
-    $ARMSubscription = [convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($AzureSubscriptionId))
-
-    Write-host "Getting GitHub Public Key to create new secrets..."
-    $GetPublicKey = @{
-        Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/actions/secrets/public-key"
-        Headers = @{
-            Authorization = "Token $($PAToken)"
-        }
-        Method = "GET"
-    }
-    $GitHubPublicKey = Invoke-RestMethod @GetPublicKey
-
-    $ARMClientIdBody = @"
+$ARMClientIdBody = @"
 {
-    "encrypted_value": "$($ARMClient)",
-    "key_id": "$($GitHubPublicKey.Key_id)"
+"encrypted_value": "$($ARMClient)",
+"key_id": "$($GitHubPublicKey.Key_id)"
 }
 "@
-    
-    Write-Host "Creating secret for ARMClient"
-    $CreateARMClientId = @{
-        Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/actions/secrets/ARM_CLIENT_ID"
-        Headers = @{
-            Authorization = "Token $($PAToken)"
-            "Content-Type" = "application/json"
-            Accept = "application/vnd.github.v3+json"
-        }
-        Body = $ARMClientIdBody
-        Method = "PUT"
-    }
-    Invoke-RestMethod @CreateARMClientId
 
-    $ARMClientSecretBody = @"
+Write-Host "Creating secret for ARMClient"
+$CreateARMClientId = @{
+    Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/actions/secrets/ARM_CLIENT_ID"
+    Headers = @{
+        Authorization = "Token $($PAToken)"
+        "Content-Type" = "application/json"
+        Accept = "application/vnd.github.v3+json"
+    }
+    Body = $ARMClientIdBody
+    Method = "PUT"
+}
+Invoke-RestMethod @CreateARMClientId
+
+$ARMClientSecretBody = @"
 {
-    "encrypted_value": "$($ARMClientSecret)",
-    "key_id": "$($GitHubPublicKey.Key_id)"
+"encrypted_value": "$($ARMClientSecret)",
+"key_id": "$($GitHubPublicKey.Key_id)"
 }
 "@
-    Write-Host "Creating secret for ARM Service Principal"
-    $CreateARMClientSecret = @{
-        Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/actions/secrets/ARM_CLIENT_SECRET"
-        Headers = @{
-            Authorization = "Token $($PAToken)"
-            "Content-Type" = "application/json"
-            Accept = "application/vnd.github.v3+json"
-        }
-        Body = $ARMClientSecretBody
-        Method = "PUT"
+Write-Host "Creating secret for ARM Service Principal"
+$CreateARMClientSecret = @{
+    Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/actions/secrets/ARM_CLIENT_SECRET"
+    Headers = @{
+        Authorization = "Token $($PAToken)"
+        "Content-Type" = "application/json"
+        Accept = "application/vnd.github.v3+json"
     }
-    Invoke-RestMethod @CreateARMClientSecret
+    Body = $ARMClientSecretBody
+    Method = "PUT"
+}
+Invoke-RestMethod @CreateARMClientSecret
 
-        $ARMTenantBody = @"
+    $ARMTenantBody = @"
 {
-    "encrypted_value": "$($ARMTenant)",
-    "key_id": "$($GitHubPublicKey.Key_id)"
+"encrypted_value": "$($ARMTenant)",
+"key_id": "$($GitHubPublicKey.Key_id)"
 }
 "@
-    Write-Host "Creating secret for ARM tenant id"
-    $CreateARMTenant = @{
-        Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/actions/secrets/ARM_TENANT_ID"
-        Headers = @{
-            Authorization = "Token $($PAToken)"
-            "Content-Type" = "application/json"
-            Accept = "application/vnd.github.v3+json"
-        }
-        Body = $ARMTenantBody
-        Method = "PUT"
+Write-Host "Creating secret for ARM tenant id"
+$CreateARMTenant = @{
+    Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/actions/secrets/ARM_TENANT_ID"
+    Headers = @{
+        Authorization = "Token $($PAToken)"
+        "Content-Type" = "application/json"
+        Accept = "application/vnd.github.v3+json"
     }
-    Invoke-RestMethod @CreateARMTenant
+    Body = $ARMTenantBody
+    Method = "PUT"
+}
+Invoke-RestMethod @CreateARMTenant
 
-    $ARMSubscriptionBody = @"
+$ARMSubscriptionBody = @"
 {
-    "encrypted_value": "$($ARMSubscription)",
-    "key_id": "$($GitHubPublicKey.Key_id)"
+"encrypted_value": "$($ARMSubscription)",
+"key_id": "$($GitHubPublicKey.Key_id)"
 }
 "@
-    Write-Host "Creating secret for ARM subscription id"    
-    $CreateARMSubscription = @{
-        Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/actions/secrets/ARM_SUBSCRIPTION_ID"
-        Headers = @{
-            Authorization = "Token $($PAToken)"
-            "Content-Type" = "application/json"
-            Accept = "application/vnd.github.v3+json"
-        }
-        Body = $ARMSubscriptionBody
-        Method = "PUT"
+Write-Host "Creating secret for ARM subscription id"    
+$CreateARMSubscription = @{
+    Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/actions/secrets/ARM_SUBSCRIPTION_ID"
+    Headers = @{
+        Authorization = "Token $($PAToken)"
+        "Content-Type" = "application/json"
+        Accept = "application/vnd.github.v3+json"
     }
-    Invoke-RestMethod @CreateARMSubscription
+    Body = $ARMSubscriptionBody
+    Method = "PUT"
+}
+Invoke-RestMethod @CreateARMSubscription
+
+# Creating AzOps Pull via dispatch
+
+$CreateDispatch = @{
+    Uri     = "https://api.github.com/repos/$($GitHubUserNameOrOrg)/$($TestRepo)/dispatches"
+    Headers = @{
+        Authorization = "Token $($PAToken)"
+        "Content-Type" = "application/json"
+        "Accept" = "application/vnd.github.everest-preview+json"
+    }
+    Body = @{
+        "event_type" = "PowerShell"
+    } | Convertto-json
+}
+Invoke-RestMethod @CreateDispatch
+}
+else
+{
+Write-Host "Repository already exist."
+}
